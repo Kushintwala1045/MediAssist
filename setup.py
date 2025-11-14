309ebf99c264e3185f412d91911775abe6139544
Kush Intwala <kushintwala05@gmail.com>
2025-11-14 21:08:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="MediAssist",
+    version="0.1.0",
+    author="Kush Intwala",
+    author_email="kushintwala05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
